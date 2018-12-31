fbde11b637a2e0093caf2ddf92169b83890e1634
Jeff Schreiner <jeff.s.schreiner@gmail.com>
2018-12-30 21:14:35 -0700

    Updated motd

--- motd.sh
+++ motd.sh
@@ -3,20 +3,20 @@ let secs=$((${upSeconds}%60))
 let mins=$((${upSeconds}/60%60))
 let hours=$((${upSeconds}/3600%24))
 let days=$((${upSeconds}/86400))
-UPTIME=`printf "%d days, %02d:%02d:%02d" "$days" "$hours" "$mins" "$secs"`
+upString=`printf "%d days, %02d:%02d:%02d" "$days" "$hours" "$mins" "$secs"`
 
 # get the load averages
-read one five fifteen rest < /proc/loadavg
+read load1 load5 load15 rest < /proc/loadavg
 
 echo "$(tput setaf 2)
-   .~~.   .~~.    `date +"%A, %e %B %Y, %r"`
-  '. \ ' ' / .'   `uname -srmo` $(tput setaf 1)
-   .~ .~~~..~.    
-  : .~.'~'.~. :   $(tput setaf 2)Uptime.............: ${UPTIME} $(tput setaf 1)
+   .~~.   .~~.    `hostname`
+  '. \ ' ' / .'   `date +"%A, %e %B %Y, %r"`
+   .~ .~~~..~.    `uname -srmo` $(tput setaf 1)
+  : .~.'~'.~. :   $(tput setaf 2)Uptime.............: ${upString} $(tput setaf 1)
  ~ (   ) (   ) ~  $(tput setaf 2)Memory.............: `free -m | awk 'FNR == 2 {print $3}'` MB (used) / `free -m | awk 'FNR == 2 {print $2}'` MB (total) $(tput setaf 1)
-( : '~'.~.'~' : ) $(tput setaf 2)Load Averages......: ${one}, ${five}, ${fifteen} (1, 5, 15 min) $(tput setaf 1)
+( : '~'.~.'~' : ) $(tput setaf 2)Load Averages......: ${load1}, ${load5}, ${load15} (1, 5, 15 min) $(tput setaf 1)
  ~ .~ (   ) ~. ~  $(tput setaf 2)Running Processes..: `ps ax | wc -l | tr -d " "` $(tput setaf 1)
   (  : '~' :  )   $(tput setaf 2)IP Addresses.......: `ifconfig eth0 | grep "inet " | xargs | cut -d " " -f 2` and `wget -qO- http://ipecho.net/plain` $(tput setaf 1)
    '~ .~~~. ~'    $(tput setaf 2)Disk Usage.........: `df -h | grep -E '^/dev/root' | awk '{ print $5 }'` on /dev/root $(tput setaf 1)
-       '~'        
+       '~'
 $(tput sgr0)"
